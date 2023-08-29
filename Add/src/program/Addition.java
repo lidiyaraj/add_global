c29a8ba8ca36e0b25209c3bd88b36172207f00cd
Lidiya <Lidiya@LAPTOP-1VL2SLMT>
2023-08-29 18:53:03 +0530

    This is a commit to add numbers

---
+++ Add/src/program/Addition.java
@@ -0,0 +1,15 @@
+package program;
+
+public class Addition {
+	public static void main(String[] args) {
+		int num1 = 24;
+		int num2 = 25;
+		System.out.println("Enter the first number:"+num1);
+		System.out.println("Enter the second number:"+num2);
+		double sum=num1+num2;
+		
+		System.out.println("Sum: "+sum);
+		
+	}
+
+}
